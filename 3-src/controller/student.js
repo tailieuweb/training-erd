82a054e49aca6d48a812bcf2fa575e984bbae9ba
Huytue <bachvonam1996@gmail.com>
2020-11-02 09:58:14 +0700

    commmit source mongodb-nodejs version 2

--- 3-src/controller/student.js
+++ 3-src/controller/student.js
@@ -1,35 +1,28 @@
-
 const Student = require("../models/student");
 const School = require("../models/school");
 const bodyParser = require("body-parser");
-
-
-
 
 module.exports.add = function (req, res) {
   School.find(function (err, dataSchool) {
     if (err) {
       res.json({ result: 0, error: err });
-    }
-    else {
+    } else {
       res.render("student/add", { schools: dataSchool });
     }
   });
-
-
-}
-
+};
 
 module.exports.insertStudent = function (req, res) {
   var students = [];
-  // for (let index = 0; index <20; index++) {
+  //for (let index = 0; index < 20000; index++) {
   let student = {
-    code: req.body.code,
-    name: req.body.name,
+    code: req.body.code + index,
+    name: req.body.name + index,
     email: req.body.email,
-    school: req.body.school
-  }
+    school: req.body.school,
+  };
   students.push(student);
+  //}
   Student.collection.insertMany(students, (err) => {
     if (err) {
       res.json({ result: 0, error: err });
@@ -38,8 +31,6 @@ module.exports.insertStudent = function (req, res) {
       res.redirect("/student");
     }
   });
-
-  // }
 };
 
 module.exports.edit = async function (req, res) {
@@ -59,7 +50,7 @@ module.exports.edit = async function (req, res) {
   });
 
   res.render("student/edit", { student: item, school: data });
-}
+};
 
 module.exports.editStudent = function (req, res) {
   Student.updateOne(
@@ -78,7 +69,7 @@ module.exports.editStudent = function (req, res) {
       }
     }
   );
-}
+};
 
 module.exports.deleteStudent = function (req, res) {
   let id = req.params.id;
@@ -89,34 +80,33 @@ module.exports.deleteStudent = function (req, res) {
       res.redirect("/student");
     }
   });
-}
-
+};
 
 module.exports.pagination = async function (req, res) {
-
   var page = parseInt(req.query.page) || 1;
-  var perPage = 2;
+  var perPage = 10;
   var drop = (page - 1) * perPage;
   var students = await Student.find();
-  var numberPage = await Student.estimatedDocumentCount() / perPage;
+  var numberPage = (await Student.estimatedDocumentCount()) / perPage;
   var start = 0;
-  var studentPerPage = await Student.find(null, null, { skip: drop }).limit(perPage);
-  var baseUrl = '?page=';
-  res.render('student/index', {
+  var studentPerPage = await Student.find(null, null, { skip: drop }).limit(
+    perPage
+  );
+  var baseUrl = "?page=";
+  res.render("student/index", {
     students: studentPerPage,
     baseUrl: baseUrl,
     page: page,
-    total: numberPage,
-    start: start
+    total: Math.ceil(numberPage),
+    start: start,
   });
-}
-
+};
 
 module.exports.search = async function (req, res) {
   var q = req.query.search;
   var students = await Student.find();
   var page = parseInt(req.query.page) || 1;
-  // Tìm kiếm 
+  // Tìm kiếm
   // searchStudent = students.filter(function (student) {
   //   numberSP++;
   //   return student.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
@@ -124,20 +114,22 @@ module.exports.search = async function (req, res) {
   var perPage = 2;
   var drop = (page - 1) * perPage;
   var end = page * perPage;
-  const studentsSearch = await Student.find({ "name": { '$regex': q } }, null, { skip: drop }).limit(perPage);
-
+  const studentsSearch = await Student.find({ name: { $regex: q } }, null, {
+    skip: drop,
+  }).limit(perPage);
 
   var start = 0;
-  var numberPage = await Student.find({ "name": { '$regex': q } }).countDocuments() / perPage;
+  var numberPage =
+    (await Student.find({ name: { $regex: q } }).countDocuments()) / perPage;
   // var numberPage = await Student.estimatedDocumentCount() / perPage;
   // var studentPerPage = searchStudent.find(null, null, { skip: drop }).limit(perPage);
-  var baseUrl = '?search=' + q + '&page=';
-  res.render('student/index', {
+  var baseUrl = "?search=" + q + "&page=";
+  res.render("student/index", {
     students: studentsSearch,
     keyword: q,
     page: page,
     baseUrl: baseUrl,
-    total: numberPage,
+    total: Math.ceil(numberPage),
     start: start,
   });
-}
+};
